4cfb27c89f23785aa3bd298717625256d297e2c8
Abraham Egnor <abraham.egnor@mongodb.com>
2024-05-20 11:39:57 -0400

    run semgrep on evergreen

--- .evergreen/check-semgrep.sh
+++ .evergreen/check-semgrep.sh
@@ -4,12 +4,15 @@ set -o errexit
 
 source ./.evergreen/configure-rust.sh
 
+. ${DRIVERS_TOOLS}/.evergreen/find-python3.sh
+PYTHON=$(find_python3)
+
 if [[ -f "semgrep/bin/activate" ]]; then
     echo 'using existing virtualenv'
     . semgrep/bin/activate
 else
     echo 'Creating new virtualenv'  
-    python3 -m venv semgrep
+    ${PYTHON} -m venv semgrep
     echo 'Activating new virtualenv'
     . semgrep/bin/activate
     python3 -m pip install semgrep
--- .evergreen/install-dependencies.sh
+++ .evergreen/install-dependencies.sh
@@ -34,3 +34,13 @@ tar xzf libmongocrypt-all.tar.gz
 if [ "Windows_NT" == "$OS" ]; then
     chmod +x ${MONGOCRYPT_LIB_DIR}/../bin/*.dll
 fi
+
+## drivers-tools
+
+if [[ -z "$DRIVERS_TOOLS" ]]; then
+    echo >&2 "\$DRIVERS_TOOLS must be set"
+    exit 1
+fi
+
+rm -rf $DRIVERS_TOOLS
+git clone https://github.com/mongodb-labs/drivers-evergreen-tools.git $DRIVERS_TOOLS
