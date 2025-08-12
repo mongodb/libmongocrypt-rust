bbbf5c81b0e08c192eafa42769203f3177c801d1
Isabel Atkinson <isabel.atkinson@mongodb.com>
2025-08-12 14:41:48 -0600

    suppress error

--- .evergreen/run-valgrind.sh
+++ .evergreen/run-valgrind.sh
@@ -4,7 +4,9 @@ set -o errexit
 
 source ./.evergreen/configure-rust.sh
 
+SUPPRESSIONS_FILE="$(pwd)/.evergreen/valgrind-suppressions.supp"
+
 cd mongocrypt
 # The test executable has a path like 'target/debug/deps/mongocrypt-661ed156b0be8130', so we scrape it from compiler output
 TEST_BIN=$(cargo build -q --tests --message-format json | sed -n 's/^.*"executable":"\(.*mongocrypt-.*\)",.*$/\1/p')
-valgrind --leak-check=yes --error-exitcode=1 $TEST_BIN
+valgrind --leak-check=yes --suppressions=$SUPPRESSIONS_FILE --error-exitcode=1 $TEST_BIN
